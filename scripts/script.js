ef2a299e261121483ed82c5e9b6e3445d81afa55
Glitch (hello-webpage) <none>
2020-03-28 01:17:52 +0000

    🚊🦂 Checkpoint ./style/style.css:2585035/23 ./scripts/script.js:2585035/197

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+words = {};
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+function calculate()
+{
+
+}
