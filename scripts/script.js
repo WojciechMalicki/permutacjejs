1380bcef81cc338b57d90523bf73932c91a928a5
Glitch (hello-webpage) <none>
2020-03-28 22:12:56 +0000

    👀🍩 Checkpoint ./scripts/script.js:2585035/453

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,28 @@
 var words = new Set();
 
+function perm(par, w)
+{
+  if (par==1)
+    {
+      words.add(w);
+    }
+  else
+    {
+      for(var i=0;i<par;i++)
+        {
+          var tmp=w[i];
+          w[i]=w[par-1];
+          w[par-1]=tmp;
+          perm(par-1)
+          tmp=w[i];
+          w[i]=w[par-1];
+          w[par-1]=tmp;
+        }
+    }
+}
+
 function calculate()
 {
-    
+    var word = document.getElementById("chartext");
+  
 }
