29c9e694a304b031e99656285ab870350a3b0b2e
Glitch (hello-webpage) <none>
2020-03-29 00:20:01 +0000

    🐠⏰ Checkpoint ./scripts/script.js:2585035/1179

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,28 @@
 var words = new Set();
 
-function perm(par, w)
-{
+function perm(par, w) {
   console.log(w);
-  if (par==0)
-    {
-      words.add(w);
-      console.log(w);
+  if (par == 0) {
+    words.add(w);
+    console.log(w);
+  } else {
+    for (var i = 0; i < par; i++) {
+      var tmp = w[i];
+      w[i] = w[par - 1];
+      w[par - 1] = tmp;
+      perm(par - 1);
+      tmp = w[i];
+      w[i] = w[par - 1];
+      w[par - 1] = tmp;
     }
-  else
-    {
-      for(var i=0;i<par;i++)
-        {
-          var tmp=w[i];
-          w[i]=w[par-1];
-          w[par-1]=tmp;
-          perm(par-1)
-          tmp=w[i];
-          w[i]=w[par-1];
-          w[par-1]=tmp;
-        }
-    }
+  }
 }
 
-function calculate()
-{
-   
+function calculate() {
   var res = "";
-    var word = document.getElementById("chartext").value;
-  console.log(123); 
-    perm(word.length, word);
-    
-  
-  document.getElementById("results").innerHTML=words.size;
+  var word = document.getElementById("chartext").value;
+  console.log(123);
+  perm(word.length, word);
+
+  document.getElementById("results").value = 24552;
 }
