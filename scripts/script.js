241a856d26d82e77c678573b7f080bceb3c2a022
Glitch (hello-webpage) <none>
2020-03-30 22:27:58 +0000

    🌉🎎 Checkpoint ./scripts/script.js:2585035/5049

--- scripts/script.js
+++ scripts/script.js
@@ -6,24 +6,22 @@ function changeL(w, b, e) {
   if (b < e) {
     if (b > 0) {
       r = r + w.slice(0, b);
-      console.log("1 " + w + ":" + b + ", " + e + " -> " + r);
     }
     r = r + w[e];
-    console.log("2 " + w + ":" + b + ", " + e + " -> " + r);
+
     if (b < e - 1) {
       r = r + w.slice(b + 1, e);
-      console.log("3 " + w + ":" + b + ", " + e + " -> " + r);
     }
 
     r = r + w[b];
-    console.log("4 " + w + ":" + b + ", " + e + " -> " + r);
+
     if (e < w.length - 1) {
       r = r + w.slice(e + 1);
-      console.log("5 " + w + ":" + b + ", " + e + " -> " + r);
     }
-    console.log("100 " + w + ":" + b + ", " + e + " -> " + r);
-    return r;
+  } else {
+    r = w;
   }
+  return r;
 }
 
 function addRes(v) {
@@ -31,17 +29,13 @@ function addRes(v) {
 }
 
 function perm(par, w) {
-  console.log("funct_perm_wyzej: " + w + ", " + par);
   if (par == 0) {
-    console.log("funct_perm: " + w);
     pwords.add(w);
     return 0;
   } else {
     for (var i = 0; i < par; i++) {
-      if (i < par - 1) {
-        var q = changeL(w, i, par - 1);
-        console.log("funct_perm_q: " + q);
-      }
+      var q = changeL(w, i, par - 1);
+
       perm(par - 1, q);
     }
   }
@@ -51,8 +45,6 @@ function calculate() {
   var pword = document.getElementById("chartext").value;
   pwords.add(pword);
   perm(pword.length, pword);
-  console.log(pwords);
   pwords.forEach(addRes);
   document.getElementById("results").innerHTML = res;
-  console.log(res);
 }
