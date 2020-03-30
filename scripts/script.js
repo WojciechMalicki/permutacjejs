3a4d73f706f94feee86b3dc450a7b9249e753ce7
Glitch (hello-webpage) <none>
2020-03-30 22:16:56 +0000

    🗻🍔 Checkpoint ./scripts/script.js:2585035/3911

--- scripts/script.js
+++ scripts/script.js
@@ -3,26 +3,27 @@ var res = "";
 
 function changeL(w, b, e) {
   var r = "";
-  if (b > 0)
-  {
-    r = r + w.slice(0, b);
-    console.log("1 " + w + ":" + b + ", " + e + " -> " + r);
-  }
-  r = r + w[e];
-  console.log("2 " + w + ":" + b + ", " + e + " -> " + r);
-  if (b < e - 1) {
-    r = r + w.slice(b + 1, e);
-    console.log("3 " + w + ":" + b + ", " + e + " -> " + r);
-  }
+  if (b < e) {
+    if (b > 0) {
+      r = r + w.slice(0, b);
+      console.log("1 " + w + ":" + b + ", " + e + " -> " + r);
+    }
+    r = r + w[e];
+    console.log("2 " + w + ":" + b + ", " + e + " -> " + r);
+    if (b < e - 1) {
+      r = r + w.slice(b + 1, e);
+      console.log("3 " + w + ":" + b + ", " + e + " -> " + r);
+    }
 
-  r = r + w[b];
-  console.log("4 " + w + ":" + b + ", " + e + " -> " + r);
-  if (e < w.length - 1) {
-    r = r + w.slice(e + 1,);
-    console.log("5 " + w + ":" + b + ", " + e + " -> " + r);
+    r = r + w[b];
+    console.log("4 " + w + ":" + b + ", " + e + " -> " + r);
+    if (e < w.length - 1) {
+      r = r + w.slice(e + 1);
+      console.log("5 " + w + ":" + b + ", " + e + " -> " + r);
+    }
+    console.log("100 " + w + ":" + b + ", " + e + " -> " + r);
+    return r;
   }
-  console.log("100 " + w + ":" + b + ", " + e + " -> " + r);
-  return r;
 }
 
 function addRes(v) {
@@ -30,18 +31,17 @@ function addRes(v) {
 }
 
 function perm(par, w) {
-   console.log("funct_perm_wyzej: " + w + ", " + par);
+  console.log("funct_perm_wyzej: " + w + ", " + par);
   if (par == 0) {
     console.log("funct_perm: " + w);
     pwords.add(w);
     return 0;
   } else {
     for (var i = 0; i < par; i++) {
-      if (i<par-1)
-        {
-          var q = changeL(w, i, par-1);
-          console.log("funct_perm_q: " + q);
-        }
+      if (i < par - 1) {
+        var q = changeL(w, i, par - 1);
+        console.log("funct_perm_q: " + q);
+      }
       perm(par - 1, q);
     }
   }
